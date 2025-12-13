2808b10c0fff1bfcf3a7ea0d5285f06fbc5e8fa8
mehdibkz82 <mehdibkz82@icloud.com>
2025-12-13 08:11:45 +0000

    File changes

--- src/components/patients/tabs/FacturationTab.jsx
+++ src/components/patients/tabs/FacturationTab.jsx
@@ -14,6 +14,7 @@ import {
 } from 'lucide-react';
 import { format } from 'date-fns';
 import { fr } from 'date-fns/locale';
+import PaymentHistory from '../../facturation/PaymentHistory';
 import {
   Select,
   SelectContent,
