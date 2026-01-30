654c67f4cf462360f1457962a9adc3e1555595a2
mehdibkz82 <mehdibkz82@icloud.com>
2026-01-30 08:55:30 +0000

    File changes

--- src/pages.config.js
+++ src/pages.config.js
@@ -87,7 +87,6 @@ import Templates from './pages/Templates';
 import Tests from './pages/Tests';
 import TiersPayant from './pages/TiersPayant';
 import Utilisateurs from './pages/Utilisateurs';
-import PatientsPage from './pages/PatientsPage';
 import Patients from './pages/Patients';
 import __Layout from './Layout.jsx';
 
@@ -133,7 +132,6 @@ export const PAGES = {
     "Tests": Tests,
     "TiersPayant": TiersPayant,
     "Utilisateurs": Utilisateurs,
-    "PatientsPage": PatientsPage,
     "Patients": Patients,
 }
 
