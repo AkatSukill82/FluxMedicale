a6dabbfb780cd76fc9eb466ed2ce6f67bb80093d
mehdibkz82 <mehdibkz82@icloud.com>
2026-03-13 13:37:28 +0000

    File changes

--- src/components/facturation/MyCareNetTable.jsx
+++ src/components/facturation/MyCareNetTable.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle } from '@/components/ui/dialog';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
